123403d768547c17146f28970d28f79693d6202d
Adam Karpierz <akarpierz@gmail.com>
2018-01-29 11:08:33 +0100

    ...
    ...

--- tests/.build.cmd
+++ tests/.build.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 pushd "%~dp0"
-if not defined JAVA_HOME ( set JAVA_HOME="C:\Program Files\Java\jdk1.8.0_152" )
-set javac=%JAVA_HOME%\bin\javac -encoding UTF-8 -g:none -deprecation -Xlint:unchecked ^
+if not defined JAVA_HOME (set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_152)
+set javac="%JAVA_HOME%"\bin\javac -encoding UTF-8 -g:none -deprecation -Xlint:unchecked ^
     -source 1.7 -target 1.7 -bootclasspath "C:\Program Files\Java\jdk1.7.0_79\jre\lib\rt.jar"
 rmdir /Q/S classes 2> nul & mkdir classes
 dir /S/B/O:N ^
